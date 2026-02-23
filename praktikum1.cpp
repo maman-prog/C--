020cb37b8d3dae9fc2f5cb9297138f1affd83a16
maman-prog <ingratarmanrizky19@gmail.com>
2026-02-23 21:14:29 +0700

    end

--- praktikum1.cpp
+++ praktikum1.cpp
@@ -3,3 +3,13 @@ using namespace std;
 
 //variable global
 int sks;
+
+int main(){
+    //variable global
+    int umur;
+    umur = 25;
+
+    cout << "Halo Tompel" << endl;
+    cout << 10 << endl;
+    cout << umur;
+}
